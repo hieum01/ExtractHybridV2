22b44d239657d87c80f8451a1ca82abe4424e0e6
hieum01 <hieum01@gmail.com>
2026-09-01 09:01:36 -0600

    Refactor Extract_HybridV2_Points function documentation

--- R/Extract_Hybrid_Points.R
+++ R/Extract_Hybrid_Points.R
@@ -61,7 +61,7 @@ Extract_HybridV2_Points<-function(List.Points,Project.Name,Path.Hybrid,
   Order.End<-which(Hybrid.Date==ed.Selected)
   #===========================================================================
   for (iVar in Target.Met.Var) {
-    Path.Hybrid.Var<-paste0(Path.Hybrid,'/Hybrid_V2_Daily_', iVar,'_1950-2019.RData')
+    Path.Hybrid.Var<-paste0(Path.Hybrid,'/Hybrid_V2_Daily_', iVar,'_1950-2024.RData')
     print(paste0("Reading data: ",iVar))
     Hybrid.Var<-get(load(Path.Hybrid.Var))
     print(paste0("Extracting data: ",iVar))
